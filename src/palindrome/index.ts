987b270467ae33023ceaced3307fff3b4caac69c
Infant Ajay Venus <infantajayv@duck.com>
2025-07-23 12:16:28 +0530

    refactor: replace emoji lookup logic with split by spread

--- src/palindrome/index.ts
+++ src/palindrome/index.ts
@@ -2,26 +2,7 @@ export function isPalindrome(phrase: string): boolean {
     const punctuationAndSpacesRegex = /[\p{P}\p{White_Space}]+/gu;
     const phraseWithoutPunctuationAndSpace = phrase.replace(punctuationAndSpacesRegex, '');
 
-    const reversePhrase = getReversedPhrase(phraseWithoutPunctuationAndSpace); 
-
+    const reversePhrase = [...phraseWithoutPunctuationAndSpace].reverse().join(''); 
 
     return phraseWithoutPunctuationAndSpace.toLowerCase() === reversePhrase.toLowerCase();
 }
-
-function getReversedPhrase(phraseWithoutPunctuationAndSpace: string) {
-    const emojiTable = {};
-    const phraseWithModifiedEmojis = phraseWithoutPunctuationAndSpace.replace(/\p{Emoji}/ug, (emoji, idx) => {
-        const index = `#${idx}#`;
-        emojiTable[index] = emoji;
-
-        return index;
-    });
-
-    const reversedPhraseWithModifiedEmojis = phraseWithModifiedEmojis.split('').reverse().join('');
-
-    const reversePhrase = reversedPhraseWithModifiedEmojis.replace(/\#\d\#/g, (index) => {
-        return emojiTable[index];
-    });
-    return reversePhrase;
-}
-
