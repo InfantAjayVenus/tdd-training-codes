281fd8bbc2a67693bf38e33d2ecdef9692e2d36c
Infant Ajay Venus <infantajayv@duck.com>
2025-07-23 12:27:41 +0530

    refactor: remove duplicate lowercasing

--- src/palindrome/index.ts
+++ src/palindrome/index.ts
@@ -1,8 +1,8 @@
 export function isPalindrome(phrase: string): boolean {
     const punctuationAndSpacesRegex = /[\p{P}\p{White_Space}]+/gu;
-    const phraseWithoutPunctuationAndSpace = phrase.replace(punctuationAndSpacesRegex, '');
+    const phraseWithoutPunctuationAndSpace = phrase.replace(punctuationAndSpacesRegex, '').toLowerCase();
 
     const reversePhrase = [...phraseWithoutPunctuationAndSpace].reverse().join(''); 
 
-    return phraseWithoutPunctuationAndSpace.toLowerCase() === reversePhrase.toLowerCase();
+    return phraseWithoutPunctuationAndSpace === reversePhrase;
 }
